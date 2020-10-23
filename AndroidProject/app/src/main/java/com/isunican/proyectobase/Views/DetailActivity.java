6374ebb214ce4051b13df4214fa68e4e61442cc1
vilav548 <vmv548@alumnos.unican.es>
2020-10-23 16:22:07 +0200

    Añadida imagen de fondo a la vista detalle.

--- AndroidProject/app/src/main/java/com/isunican/proyectobase/Views/DetailActivity.java
+++ AndroidProject/app/src/main/java/com/isunican/proyectobase/Views/DetailActivity.java
@@ -3,10 +3,13 @@ package com.isunican.proyectobase.Views;
 import com.isunican.proyectobase.R;
 import com.isunican.proyectobase.Model.*;
 
+import android.content.Context;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.text.TextUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
@@ -28,6 +31,9 @@ public class DetailActivity extends AppCompatActivity {
     TextView localidad;
     TextView precioGasoil;
     TextView precioGasolina;
+
+    ImageView imagenGasolinera;
+    Context context;
 
     Button btnFavoritos;
     Button btnMapa;
@@ -59,17 +65,36 @@ public class DetailActivity extends AppCompatActivity {
         precioGasolina = findViewById(R.id.textView8);
         precioGasoil = findViewById(R.id.textView10);
 
+        imagenGasolinera = findViewById(R.id.imageViewGasolinera);
+
         g = getIntent().getExtras().getParcelable(getResources().getString(R.string.pasoDatosGasolinera));
         //CAMPOS DE LA TABLA
         rotulo.setText(g.getRotulo());
         direccion.setText(g.getDireccion());
         localidad.setText(g.getLocalidad());
-        precioGasolina.setText(Double.toString(g.getGasolina95()));
-        precioGasoil.setText(Double.toString(g.getGasoleoA()));
+        precioGasolina.setText(Double.toString(g.getGasolina95()) + "€");
+        precioGasoil.setText(Double.toString(g.getGasoleoA()) + "€");
+
+        context = this;
 
         btnFavoritos = findViewById(R.id.btnFavoritos);
         btnMapa = findViewById(R.id.btnMapa);
-        //AQUI LES ASIGNARIAMOS UN ACTION LISTENER
+        //TODO AQUI LES ASIGNARIAMOS UN ACTION LISTENER
+
+        // Cargamos el icono de la gasolinera, aprovechando el código de
+        //la clase MainActivity.
+        //TODO Mas adelante, en el informe de calidad, esto puede detectarse como código duplicado,
+        //por lo que igual hay que hacer un método
+        {
+            String rotuleImageID = g.getRotulo().toLowerCase();
+            int imageID = context.getResources().getIdentifier(rotuleImageID,
+                    "drawable", context.getPackageName());
+            if (imageID == 0 || TextUtils.isDigitsOnly(rotuleImageID)) {
+                imageID = context.getResources().getIdentifier(getResources().getString(R.string.pordefecto),
+                        "drawable", context.getPackageName());
+            }
+            imagenGasolinera.setImageResource(imageID);
+        }
 
 
     }
