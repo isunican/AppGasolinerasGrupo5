be4960b919fede21575927223409b9d39f22fd78
PabloVillegas95 <pablovillef@gmail.com>
2020-11-01 21:39:08 +0100

    Se ha creado el esqueleto para poder implementar las pruebas unitarias. Ademas, se han implementado parcialmente las pruebas de interfaz, a falta de comprobar que los cambios se han realizado correctamente en espresso

---
+++ AndroidProject/app/src/test/java/com/isunican/proyectobase/Model/FiltroTest.java
@@ -0,0 +1,8 @@
+package com.isunican.proyectobase.Model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FiltroTest {
+}
---
+++ AndroidProject/app/src/test/java/com/isunican/proyectobase/Presenter/PresenterGasolinerasTest.java
@@ -0,0 +1,8 @@
+package com.isunican.proyectobase.Presenter;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PresenterGasolinerasTest {
+}
