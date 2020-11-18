57edb0cf5344e160fbf1e0738b5c43d133838ec1
vilav548 <vmv548@alumnos.unican.es>
2020-11-18 17:42:30 +0100

    Añadido paquete de pruebas.

---
+++ AndroidProject/app/src/AndroidTest/399114-OrdenarPorPrecio/PresenterGasolinerasTest.java
@@ -0,0 +1,26 @@
+
+
+public class PresenterGasolinerasTest {
+
+    PresenterGasolineras presenter;
+
+    @Test
+    public void cargaDatosRemotosTest(){
+
+    }
+
+    @Test
+    public void ordenarPorPrecioMenorAMayorTest(){
+
+    }
+
+    @Test
+    public void ordenarPorPrecioMayorAMenorTest(){
+
+    }
+
+    @Test
+    public void ordenarPorPrecioTest(){
+
+    }
+}
