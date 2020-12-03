72500a5855cc7634a8d39408d702eeb6a4d79ae1
Pablovillegas95 <pablovillef@gmail.com>
2020-12-03 02:25:32 +0100

    modificada estructura test sin switch

--- AndroidProject/app/src/androidTest/java/com/isunican/proyectobase/MostrarPorOrdenAlfabeticoFiltroDeMarcasUI/MostrarPorOrdenAlfabeticoFiltroDeMarcasUITest.java
+++ AndroidProject/app/src/androidTest/java/com/isunican/proyectobase/MostrarPorOrdenAlfabeticoFiltroDeMarcasUI/MostrarPorOrdenAlfabeticoFiltroDeMarcasUITest.java
@@ -17,6 +17,7 @@ import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withSpinnerText;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.anything;
 import static org.hamcrest.Matchers.containsString;
 
@@ -25,180 +26,68 @@ import static org.hamcrest.Matchers.containsString;
 public class MostrarPorOrdenAlfabeticoFiltroDeMarcasUITest {
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
-
-
+    
     @Test
-    public void marcaPorDefectoUITest(){
-        //Primero vamos a la vista de seleccionar filtros y clickamos el boton de guardar configuracion
+    public void marcasOrdenadasUITest(){
+        //Primero vamos a la vista de seleccionar filtros
         onView(withId(R.id.btnListaFiltros)).perform(click());
+        // Seleccionamos el spinner parar ver las marcas
+        onView(withId(R.id.spinnerMarca)).perform(click());
 
-        for(int i = 0; i < 53; i++){
-            //Comprobar que el primero es "Todas"
-            onView(withId(R.id.spinnerMarca)).perform(click());
-            onData(anything()).atPosition(i).perform(scrollTo(), click());
-
-            switch(i){
-                case 0:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("Todas"))));
-                    break;
-                case 1:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString(" SHELL"))));
-                    break;
-                case 2:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("(SIN RÓTULO)"))));
-                    break;
-                case 3:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("12241"))));
-                    break;
-                case 4:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("AGROCANTABRIA"))));
-                    break;
-                case 5:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("AREA DE SERVICIO LA AGÜERA"))));
-                    break;
-                case 6:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("AREA DE SERVICIO LA PALMERA"))));
-                    break;
-                case 7:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("AREA DE SERVICIO PAMANES"))));
-                    break;
-                case 8:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("AREA DE SERVICIO TORRELAVEGA"))));
-                    break;
-                case 9:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("AVIA"))));
-                    break;
-                case 10:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("BEROIL"))));
-                    break;
-                case 11:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("BP CASTRO URDIALES"))));
-                    break;
-                case 12:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("BP LA OLA MARINA"))));
-                    break;
-                case 13:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("BP MERCASANTANDER"))));
-                    break;
-                case 14:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("BP POLANCO"))));
-                    break;
-                case 15:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("CAMPIEZO -AVIA"))));
-                    break;
-                case 16:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("CAMPSA"))));
-                    break;
-                case 17:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("CARREFOUR"))));
-                    break;
-                case 18:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("CEPSA"))));
-                    break;
-                case 19:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("COBO"))));
-                    break;
-                case 20:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("COMBUSCAN"))));
-                    break;
-                case 21:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("E.S. ARGOMILLA"))));
-                    break;
-                case 22:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("E.S. CARBURANTES DE ARNUERO S.L."))));
-                    break;
-                case 23:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("E.S. ISLARES S.L."))));
-                    break;
-                case 24:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("E.S. MARJA"))));
-                    break;
-                case 25:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("E.S. SOMAHOZ"))));
-                    break;
-                case 26:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("E.S. TRECEÑO S.L."))));
-                    break;
-                case 27:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("E.S. TRINIDAD"))));
-                    break;
-                case 28:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("E.S.NUEVA MONTAÑA"))));
-                    break;
-                case 29:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("E.S.SOLBAS"))));
-                    break;
-                case 30:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("EASYGAS"))));
-                    break;
-                case 31:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("EESS LA CAVADA"))));
-                    break;
-                case 32:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("EESSMAS, S.L."))));
-                    break;
-                case 33:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("EL CENTRO"))));
-                    break;
-                case 34:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("EROSKI"))));
-                    break;
-                case 35:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("ES FERMAN LIAÑO"))));
-                    break;
-                case 36:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("ESTACION DE SERVICIO LIEBANA S.L"))));
-                    break;
-                case 37:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("EXPOMARINA"))));
-                    break;
-                case 38:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("G2"))));
-                    break;
-                case 39:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("GALP"))));
-                    break;
-                case 40:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("MEROIL"))));
-                    break;
-                case 41:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("NIEVES"))));
-                    break;
-                case 42:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("PARTE"))));
-                    break;
-                case 43:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("PETROLEOS DE CUDEYO"))));
-                    break;
-                case 44:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("PETRONOR"))));
-                    break;
-                case 45:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("REPOSTA BEZANA"))));
-                    break;
-                case 46:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("REPSOL"))));
-                    break;
-                case 47:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("RUYCO PETROL"))));
-                    break;
-                case 48:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("RUYCOPETROL, S.L."))));
-                    break;
-                case 49:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("SHELL"))));
-                    break;
-                case 50:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("SHELL"))));
-                    break;
-                case 51:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("STAR PETROLEUM"))));
-                    break;
-                case 52:
-                    onView(withId(R.id.spinnerMarca)).check(matches(withSpinnerText(containsString("VALLE DE LIENDO"))));
-                    break;
-            }
+        // Comprobamos que estan ordenadas alfabeticamente
+        onData(anything()).atPosition(0).check(matches(withText("Todas")));
+        onData(anything()).atPosition(1).check(matches(withText(" SHELL")));
+        onData(anything()).atPosition(2).check(matches(withText("(SIN RÓTULO)")));
+        onData(anything()).atPosition(3).check(matches(withText("12241")));
+        onData(anything()).atPosition(4).check(matches(withText("AGROCANTABRIA")));
+        onData(anything()).atPosition(5).check(matches(withText("AREA DE SERVICIO LA AGÜERA")));
+        onData(anything()).atPosition(6).check(matches(withText("AREA DE SERVICIO LA PALMERA")));
+        onData(anything()).atPosition(7).check(matches(withText("AREA DE SERVICIO PAMANES")));
+        onData(anything()).atPosition(8).check(matches(withText("AREA DE SERVICIO TORRELAVEGA")));
+        onData(anything()).atPosition(9).check(matches(withText("AVIA")));
+        onData(anything()).atPosition(10).check(matches(withText("BEROIL")));
+        onData(anything()).atPosition(11).check(matches(withText("BP CASTRO URDIALES")));
+        onData(anything()).atPosition(12).check(matches(withText("BP LA OLA MARINA")));
+        onData(anything()).atPosition(13).check(matches(withText("BP MERCASANTANDER")));
+        onData(anything()).atPosition(14).check(matches(withText("BP POLANCO")));
+        onData(anything()).atPosition(15).check(matches(withText("CAMPIEZO -AVIA")));
+        onData(anything()).atPosition(16).check(matches(withText("CAMPSA")));
+        onData(anything()).atPosition(17).check(matches(withText("CARREFOUR")));
+        onData(anything()).atPosition(18).check(matches(withText("CEPSA")));
+        onData(anything()).atPosition(19).check(matches(withText("COBO")));
+        onData(anything()).atPosition(20).check(matches(withText("COMBUSCAN")));
+        onData(anything()).atPosition(21).check(matches(withText("E.S. ARGOMILLA")));
+        onData(anything()).atPosition(22).check(matches(withText("E.S. CARBURANTES DE ARNUERO S.L.")));
+        onData(anything()).atPosition(23).check(matches(withText("E.S. ISLARES S.L.")));
+        onData(anything()).atPosition(24).check(matches(withText("E.S. MARJA")));
+        onData(anything()).atPosition(25).check(matches(withText("E.S. SOMAHOZ")));
+        onData(anything()).atPosition(26).check(matches(withText("E.S. TRECEÑO S.L.")));
+        onData(anything()).atPosition(27).check(matches(withText("E.S. TRINIDAD")));
+        onData(anything()).atPosition(28).check(matches(withText("E.S.NUEVA MONTAÑA")));
+        onData(anything()).atPosition(29).check(matches(withText("E.S.SOLBAS")));
+        onData(anything()).atPosition(30).check(matches(withText("EASYGAS")));
+        onData(anything()).atPosition(31).check(matches(withText("EESS LA CAVADA")));
+        onData(anything()).atPosition(32).check(matches(withText("EESSMAS, S.L.")));
+        onData(anything()).atPosition(33).check(matches(withText("EL CENTRO")));
+        onData(anything()).atPosition(34).check(matches(withText("EROSKI")));
+        onData(anything()).atPosition(35).check(matches(withText("ES FERMAN LIAÑO")));
+        onData(anything()).atPosition(36).check(matches(withText("ESTACION DE SERVICIO LIEBANA S.L")));
+        onData(anything()).atPosition(37).check(matches(withText("EXPOMARINA")));
+        onData(anything()).atPosition(38).check(matches(withText("G2")));
+        onData(anything()).atPosition(39).check(matches(withText("GALP")));
+        onData(anything()).atPosition(40).check(matches(withText("MEROIL")));
+        onData(anything()).atPosition(41).check(matches(withText("NIEVES")));
+        onData(anything()).atPosition(42).check(matches(withText("PARTE")));
+        onData(anything()).atPosition(43).check(matches(withText("PETROLEOS DE CUDEYO")));
+        onData(anything()).atPosition(44).check(matches(withText("PETRONOR")));
+        onData(anything()).atPosition(45).check(matches(withText("REPOSTA BEZANA")));
+        onData(anything()).atPosition(46).check(matches(withText("REPSOL")));
+        onData(anything()).atPosition(47).check(matches(withText("RUYCO PETROL")));
+        onData(anything()).atPosition(48).check(matches(withText("RUYCOPETROL, S.L.")));
+        onData(anything()).atPosition(49).check(matches(withText("SHELL")));
+        onData(anything()).atPosition(50).check(matches(withText("SHELL ")));
+        onData(anything()).atPosition(51).check(matches(withText("STAR PETROLEUM")));
+        onData(anything()).atPosition(52).check(matches(withText("VALLE DE LIENDO")));
         }
     }
-}
 
